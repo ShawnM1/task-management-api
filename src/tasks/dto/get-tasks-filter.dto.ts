16e5585f368aab9a3463fb1351a59c8a13aa8636
shawn_mccall <mccalls2@yahoo.com>
2023-04-07 18:54:05 -0400

    Lecture 3-35: update validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,14 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
+import { isStringObject } from "util/types";
+
 
 export class GetTasksFilterDto {
-    status?: TaskStatus
-    search?: string
+    @IsOptional()
+    @IsEnum(TaskStatus)
+    status: TaskStatus
+    
+    @IsOptional()
+    @IsString()
+    search: string
 }
